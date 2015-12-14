7d8c4d389c2e1e738e209bf8a0b7228536e4c483
Foton <dcherkasov256@gmail.com>
2015-12-15 01:22:01 +0300

    Default gulp task. Building css and js

--- frontgoose/gulpfile.js
+++ frontgoose/gulpfile.js
@@ -1,29 +1,34 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify');
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    autoprefixer = require("gulp-autoprefixer"),
+    minifycss = require("gulp-minify-css"),
+    rename = require("gulp-rename"),
+    concat = require("gulp-concat"),
+    uglify = require("gulp-uglify");
 
-gulp.task('process-styles', function() {
-    return gulp.src('./scss/*.scss')
+gulp.task("process-styles", function() {
+    return gulp.src("scss/*.scss")
         .pipe(sass({
-            //style: 'expanded',
-            includePaths: ['bower_components/foundation/scss']
+            style: "expanded",
+            includePaths: ["bower_components/foundation/scss"]
         }))
-        //.pipe(autoprefixer('last 2 version'))
-        .pipe(gulp.dest('./css/'))
-        //.pipe(rename({suffix: '.min'} ))
-        //.pipe(minifycss())
-        //.pipe(gulp.dest('css/'))
+        .pipe(autoprefixer("last 2 version"))
+        .pipe(gulp.dest("css/"))
+        .pipe(rename({suffix: ".min"} ))
+        .pipe(minifycss())
+        .pipe(gulp.dest("css/"))
 });
 
-gulp.task('process-scripts', function() {
-    return gulp.src('src/scripts/*.js')
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('dest/scripts/'))
-        .pipe(rename({suffix: '.min'}))
+gulp.task("process-scripts", function() {
+    return gulp.src("js/src/*.js")
+        .pipe(concat("app.js"))
+        .pipe(gulp.dest("js/"))
+        .pipe(rename({suffix: ".min"}))
         .pipe(uglify())
-        .pipe(gulp.dest('dest/scripts/'))
+        .pipe(gulp.dest("js/"))
+});
+
+gulp.task("default", function() {
+    gulp.watch("scss/*.scss", ["process-styles"])
+    gulp.watch("js/src/*.js", ["process-scripts"])
 });
